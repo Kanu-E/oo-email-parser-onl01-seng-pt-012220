69d29db68db771a25e71d557781a0c34d345aa1d
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-03-16 21:47:07 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,6 +3,8 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailParser
+  
+  attr_accessor
   def parse 
     csv_emails.split.collect do |address|
       address.split(',') 
